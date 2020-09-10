4f89760441e0a1dee12db3df257844663fee5bd5
Aniket Kumar <aniketkumar2034@gmail.com>
2020-09-10 21:22:23 +0530

    Some changes

--- StockMarketLibrary/IPODetails.cs
+++ StockMarketLibrary/IPODetails.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StockMarketLibrary
 {
